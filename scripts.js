e1ec4294172932bdebc93cae53c5d70c19b8a972
yodir <balas.peter@gmail.com>
2020-04-30 17:31:26 +0200

    jquery beillesztese

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
+// Third part
+
+$('h1').html('This is done by <em>Javascript!</em>');
+$('p').css('border', '1px solid red');
+$('p').css('padding', '10px');
+$('h2').toggleClass ('blue');
+// Masodszor leirva kiveszi a class-ot
+$('h2').toggleClass ('blue');
+
 console.log('Script file is loaded.');
+
+//Single line comment
+
+/* 
+Multiple
+lines
+of comments
+*/
+
