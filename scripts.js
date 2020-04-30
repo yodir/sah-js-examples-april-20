7a5b6ce88445e553ef0c99716b173b536be4a193
yodir <balas.peter@gmail.com>
2020-04-30 17:56:16 +0200

    Third part

--- scripts.js
+++ scripts.js
@@ -1,11 +1,28 @@
 // Third part
 
 $('h1').html('This is done by <em>Javascript!</em>');
-$('p').css('border', '1px solid red');
-$('p').css('padding', '10px');
+
 $('h2').toggleClass ('blue');
 // Masodszor leirva kiveszi a class-ot
 $('h2').toggleClass ('blue');
+
+let name ='Adam';
+age = 34;
+
+function myLittleFunction() {
+    $('main').append('<p>A p is appended in the main</p>');
+    $('p').css('border', '1px solid red');
+    $('p').css('padding', '10px');
+}
+
+myLittleFunction();
+myLittleFunction();
+
+$('#add').click(myLittleFunction);
+$('#remove').click(function () {
+    $('p:last-child').remove();
+});
+
 
 console.log('Script file is loaded.');
 
